77c8f3b373cd4c4b8a5721c73ae3427989dabd9c
trent <b.trent5@gmail.com>
2017-09-09 13:41:59 -0600

    converter done

--- converter.py
+++ converter.py
@@ -1,14 +1,40 @@
+##open files
 input = open('abalone/abalone.data', 'r')
-output = open('output.data', 'w')
+open('output.data', 'w')
+output = open('output.data', 'a')
 
+##count how many attributes based on commas
 line = input.readline()
-numAttributes = line.count(line, ',')
+numAttributes = line.count(',') + 1
 
-output.append('@REALATION ')
-output.append(raw_input('Please enter the relation name: '))
+##header
+output.write('@REALATION ')
+output.write(raw_input('Please enter the relation name: ') + '\n')
 
-# output.append('@DATA ')
-# for (i = 0, i <= numAttributes + 1 , i++)
+x = 0
+while (x < numAttributes):
+    name = raw_input('Please enter attribute name (' + str(x+1) + '/' + str(numAttributes) +'): ')
+    type = raw_input('Please enter the data type for "' + name + '": ')
 
+##    shortcuts
+    if type == 'nu':
+        type = 'NUMERIC'
+    elif type == 'no':
+        type = 'NOMINAL'
+    elif type == 'st':
+        type = 'STRING'
+    elif type == 'da':
+        type = 'DATE'
+
+    output.write('\n@ATTRIBUTE ' + str(name) + ' ' + str(type) )
+
+    x+= 1
+
+##copy over data from input file
+output.write('\n\n@DATA\n' + line)
+for line in input:
+    output.write(line)
+
+##tidying up
 input.close()
 output.close()
