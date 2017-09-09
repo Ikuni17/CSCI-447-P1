98a37fba45e92327762ea5c520923abe05d57a35
Trent <b.trent5@gmail.com>
2017-09-09 12:33:58 -0600

    some converter changes, no real progress

--- converter.py
+++ converter.py
@@ -1,11 +1,14 @@
-input = open('abalone/abalone.data', r)
-output = open('output.data', w)
+input = open('abalone/abalone.data', 'r')
+output = open('output.data', 'w')
 
 line = input.readline()
 numAttributes = line.count(line, ',')
 
 output.append('@REALATION ')
-output.append(raw_input('Please enter the relation name: ')
+output.append(raw_input('Please enter the relation name: '))
 
-output.append('\n @DATA
-for (i = 0, i <= numAttributes + 1 , i++)
+# output.append('@DATA ')
+# for (i = 0, i <= numAttributes + 1 , i++)
+
+input.close()
+output.close()
